6daf849c7bf08d910d447bc58589256d96c180ee
neqw1te <ivansokol457@gmail.com>
2025-11-13 02:20:39 +0500

    added competencies vectorizin

--- src/recommendation/students/__init__.py
+++ src/recommendation/students/__init__.py
@@ -1,3 +1,17 @@
 from src.recommendation.students.clustering import clusterize_directions
+from src.recommendation.students.profile_generator import (
+    generate_profile_description,
+    vectorize_student_profile,
+    vectorize_profiles_batch,
+    update_profile_embedding_from_competencies,
+    categorize_competency,
+)
 
-__all__ = ["clusterize_directions"]
+__all__ = [
+    "clusterize_directions",
+    "generate_profile_description",
+    "vectorize_student_profile",
+    "vectorize_profiles_batch",
+    "update_profile_embedding_from_competencies",
+    "categorize_competency",
+]
